073b4fb0b626cc453c811cb6b8f247b3dd7725de
Mario <marioskrlec222@gmail.com>
2023-07-13 10:59:11 +0200

    Made types part of the package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 import {useCallback, useEffect, useRef} from 'react';
 
+interface UseAdvancedTimer {
+	maxTicks: number;
+	interval: number;
+	onTick?(): void;
+	onExit?(): void;
+	onElapsed?(): void;
+	onRestart?(): void;
+}
+interface TimerInfo {
+	totalTicks: number;
+	numOfRestarts: number;
+	numOfElapsed: number;
+	numOfExits: number;
+}
+
+type VoidFn = () => void;
+type InfoFn = () => TimerInfo;
+type UpdatePropsFn = (interval: number, maxTicks: number) => void;
+
 export default function useAdvancedTimer({
 	onTick,
 	onExit,
